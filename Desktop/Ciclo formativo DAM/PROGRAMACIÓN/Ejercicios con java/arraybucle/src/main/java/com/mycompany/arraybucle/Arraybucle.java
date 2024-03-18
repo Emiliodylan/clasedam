e718c261db1ad2b5094871ce4b88718df373a1da
Emilio <emiliodylan@gmail.com>
2024-03-18 19:29:29 +0100

    start proyect

---
+++ Desktop/Ciclo formativo DAM/PROGRAMACIÓN/Ejercicios con java/arraybucle/src/main/java/com/mycompany/arraybucle/Arraybucle.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.arraybucle;
+
+
+public class Arraybucle {
+
+    public static void main(String[] args) {
+        int numeros[]=new int[5];
+     for(int contador=0;contador<5;contador++) 
+     {
+         System.out.println(numeros[contador]);
+     }
+         
+    }
+}
